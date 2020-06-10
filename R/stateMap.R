7ffd64d87533bd318031e5d620a2163a4343bda0
tinaisat <63061577+tinaisat@users.noreply.github.com>
2020-06-10 12:21:39 -0700

    removing all things county

--- R/stateMap.R
+++ R/stateMap.R
@@ -4,7 +4,6 @@
 #' 
 #' @param data A dataframe containing values to plot a county map. At a minimum
 #' there will be stateCode, countyName, and countyFIPS.
-#' @param county_SPDF Vector of US counties.
 #' @param state_SPDF ector of US States. 
 #' @param paletteName A palette name or a vector of colors based on RColorBrewer.
 #' @param breaks If style is fixed, breaks can be specified. 
@@ -12,7 +11,6 @@
 #' @param stateCode Vector of state codes.
 #' @param projection Specified method to represent surface of Earth.
 #' @param stateBorderColor The color of the state border to display.
-#' @param countyBorderColor The color of the county borders to display.
 #' @return A ggplot object.
 #' 
 #' @rdname stateMap
@@ -30,20 +28,16 @@
 #' @importFrom rlang .data
 #' 
 
-stateMap <- function(
-  data = NULL,
-  county_SPDF = "USCensusCounties_05",
-  state_SPDF = "USCensusStates",
-  paletteName = "YlOrBr",
-  #style = ifelse(is.null(breaks), "pretty", "fixed"),
-  breaks = NULL,
-  conusOnly = TRUE,
-  stateCode = NULL,
-  projection = NULL,
-  stateBorderColor = "gray50",
-  countyBorderColor = "white"
-) {
-  
+  stateMap <- function(
+    data = NULL,
+    state_SPDF = "USCensusStates",
+    paletteName = "YlOrBr",
+    breaks = NULL,
+    conusOnly = TRUE,
+    stateCode = NULL,
+    projection = NULL,
+    stateBorderColor = "gray50"
+  ) {
   # ----- Validate parameters --------------------------------------------------
   
   MazamaCoreUtils::stopIfNull(data)
@@ -51,10 +45,6 @@ stateMap <- function(
   # Check if data exists
   if ( !exists("data") )
     stop("Parameter 'data' is empty.")
-  
-  # Accept county SPDF as character string or as object 
-  if ( is.character(county_SPDF) ) 
-    county_SPDF <- get(county_SPDF)
   
   # Accept state SPDF as character string or as object
   if ( is.character(state_SPDF) ) 
@@ -74,66 +64,48 @@ stateMap <- function(
   
   # ----- Prepare data ---------------------------------------------------------
   
+  # For US state code, can find if it matches MazamaSpatialUtils::US_52 (Vector of 52 elements) 
+  all(data$stateCode %in% MazamaSpatialUtils::US_52) 
+  nomatchStateCodes <- setdiff(data$stateCode, state_SPDF$stateCode)
+  nomatchStateNames <- setdiff(data$stateName, state_SPDF$stateName)
+  
+  # Check to see if stateCode is length 2. If not, return non-match.
+  if ( !all(stringr::str_count(data$stateCode) == "2") ) {
+    stop(paste0("Non-matching state codes: ", paste0(nomatchStateCodes, collapse = ", ")))
+  }
+  
+  # Check if state names match. If not return non-match.
+  if ( !all(data$stateName %in% state_SPDF$stateName) ) {
+    stop(paste0("Non-matching state names: ", paste0(nomatchStateNames, collapse = ", ")))
+  }
+  
   if ( !is.null(stateCode) ) {
-    county_SPDF <- subset(county_SPDF, county_SPDF$stateCode %in% stateCode)
     state_SPDF <- subset(state_SPDF, state_SPDF$stateCode %in% stateCode)
     data <- data %>% dplyr::filter(.data$stateCode %in% stateCode)
   } else if ( conusOnly ) {
-    county_SPDF <- subset(county_SPDF, county_SPDF$stateCode %in% MazamaSpatialUtils::CONUS)
     state_SPDF <- subset(state_SPDF, state_SPDF$stateCode %in% MazamaSpatialUtils::CONUS)
     data <- data %>% dplyr::filter(.data$stateCode %in% MazamaSpatialUtils::CONUS)
   } else {
-    county_SPDF <- county_SPDF 
     state_SPDF <- state_SPDF
     data <- data
   }
   
   # * Project data -----
   
-  if ( !is.null(proj) ) {
-    county_SPDF@proj4string <- sp::CRS("+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")
+  if ( !is.null(projection) ) {
     state_SPDF@proj4string <- sp::CRS("+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")
     } else if ( conusOnly ) {
-    conus_proj <- sp::CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs")
+      conus_proj <- sp::CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs")
     } else {
-    county_SPDF@proj4string <- sp::CRS("+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")
-  }
+      state_SPDF@proj4string <- sp::CRS("+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")
+    }
 
   # Breaks Need to add in style in order to make this work? 
   if ( !is.null(breaks) ) {
     stop("Please specify breaks.")
   }
-  
-  #############################################################
-  #############################################################
-  #############################################################
-  
+ 
   # ----- Merge data with SPDF -------------------------------------------------
-  
-  # For US state code, can find if it matches MazamaSpatialUtils::US_52 (Vector of 52 elements) 
-  
-  all(data$stateCode %in% MazamaSpatialUtils::US_52) 
-  
-  setdiff(data$countyName, county_SPDF$countyName)
-  setdiff(data$countyFIPS, county_SPDF$countyFIPS) # Interesting... length 3 vs length 5
-  setdiff(data$stateCode, state_SPDF$stateCode)
-  
-  # Check to see if stateCode is length 2. If not, which 
-  if ( !any(stringr::str_count(data$stateCode) == "2") ) {
-    stop("Length of stateCode should be [2].")
-  } else {
-    setdiff(data$stateCode, state_SPDF$stateCode)
-  }
-  
-  
-  if ( !data$stateName %in% state_SPDF$stateName) {
-    stop("State names do not match up.")
-  }else{
-    setdiff(data$stateName, state_SPDF$stateName)
-  }
-  
-  
-  # For the below, is Jon asking that
   
   # TODO:  Should have a way to support either 'countyFIPS' or
   # TODO:  'stateCode'+'countyName'. We will soon have utility functions in
@@ -141,13 +113,12 @@ stateMap <- function(
   
   # ----- Create plot ----------------------------------------------------------
   
-  # TODO:  Using 'local_jon/JHU_deths.R' as an example, put together an 
-  # TODO:  attractive map.
-  
   # Most basic map
   # tm_shape(data) +
   #  tm_polygons()
-  
+  #############################################################
+  # making corrections right now to join datasets - wip 
+  #############################################################
   stateMap <-
     tm_shape(data) +
     tm_polygons(
@@ -177,27 +148,27 @@ stateMap <- function(
 
 # ===== DEBUGGING ==============================================================
 
-if ( FALSE ) {
+  if ( FALSE ) {
+    
+    library(MazamaSpatialPlots)
+    mazama_initialize()
+    
+    loadSpatialData("USCensusCounties_05")
+    
+    # Set up required variables so we can walk through the code
+    data = USCensusCounties@data
+    state_SPDF = "USCensusStates"
+    paletteName = "YlOrBr"
+    #style = ifelse(is.null(breaks), "pretty", "fixed"),
+    breaks = NULL
+    conusOnly = TRUE
+    stateCode = NULL
+    projection = NULL
+    stateBorderColor = "gray50"
+    countyBorderColor = "white"
+    
+    # Run the code above and then start walking through the lines of code in the
+    # function.
+    
+  }
   
-  library(MazamaSpatialPlots)
-  mazama_initialize()
-  
-  loadSpatialData("USCensusCounties_05")
-  
-  # Set up required variables so we can walk through the code
-  data = USCensusCounties@data
-  county_SPDF = "USCensusCounties"
-  state_SPDF = "USCensusStates"
-  paletteName = "YlOrBr"
-  #style = ifelse(is.null(breaks), "pretty", "fixed"),
-  breaks = NULL
-  conusOnly = TRUE
-  stateCode = NULL
-  projection = NULL
-  stateBorderColor = "gray50"
-  countyBorderColor = "white"
-  
-  # Run the code above and then start walking through the lines of code in the
-  # function.
-  
-}
